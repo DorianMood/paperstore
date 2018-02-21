6a05080b35b2e9d811f28f2ba5e8839d9a8d6628
nekithockey75 <nikita01051997@gmail.com>
2018-02-21 12:52:54 +0500

    Everything done. Added comments and footer.

--- src/bottom/index.jsx
+++ src/bottom/index.jsx
@@ -9,10 +9,8 @@ class Bottom extends Component {
       <hr className="rgba-white-light" id="top" />
       <div className="row d-flex text-center justify-content-center mb-md-0 mb-4">
       <div className="col-md-8 col-12 mt-5">
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-      vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-      aut fugit, sed quia consequuntur.</p>
+      <h3>Сделано для вас</h3>
+      <p>Мы предлагаем шаблоны и инструкции для того чтобы дать вам возможность превратить обыкновенную бумагу в замечательную 3D маску, которую вы можете сделать и украсить сами. Инструкции очень простые и кто угодно сможет собрать красовое оригами, используя то что мы поставляем в наборе. Впеерд!</p>
       </div>
       </div>
       <hr className="clearfix d-md-none rgba-white-light" id="bottom" />
